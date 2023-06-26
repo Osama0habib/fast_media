147470a234a5c4da8cfb0ddd8ea18d22420b3485
ahmed hamdi <ahmedhamdi725@gmail.com>
2023-06-26 09:01:11 +0300

    show more or less in cast gridview

--- lib/home/presentaion/widgets/movie_cast.dart
+++ lib/home/presentaion/widgets/movie_cast.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fast_media/colors/colors.dart';
 import 'package:fast_media/core/constants/api_constants.dart';
+import 'package:fast_media/home/domain/entities/cast.dart';
 import 'package:fast_media/home/presentaion/bloc/movie_details_bloc.dart';
 import 'package:fast_media/syles/app_styles.dart';
 import 'package:flutter/material.dart';
@@ -7,9 +9,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../core/constants/api_enums.dart';
 
-class MovieCast extends StatelessWidget {
+class MovieCast extends StatefulWidget {
   const MovieCast({super.key});
 
+  @override
+  State<MovieCast> createState() => _MovieCastState();
+}
+
+class _MovieCastState extends State<MovieCast> {
+  bool isExpanded = false;
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<MovieDetailsBloc, MovieDetailsState>(
@@ -42,43 +50,39 @@ class MovieCast extends StatelessWidget {
                       mainAxisSpacing: 12.0,
                     ),
                     scrollDirection: Axis.vertical,
-                    itemCount: state.cast.length,
+                    itemCount: isExpanded ? state.cast.length : 4,
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
                     itemBuilder: (context, index) {
-                      return LayoutBuilder(
-                        builder: (context, constraints) => Container(
-                          padding: EdgeInsets.all(constraints.maxWidth * 0.05),
-                          decoration: BoxDecoration(
-                            color: kWireframe_4,
-                            borderRadius: BorderRadius.circular(16.0),
-                          ),
-                          child: Row(children: [
-                            CircleAvatar(
-                              radius: constraints.maxWidth * 0.15,
-                              backgroundImage: NetworkImage(
-                                ApiConstant.imageUrl(
-                                    path: state.cast[index].profilePath),
-                              ),
-                            ),
-                            SizedBox(
-                              width: constraints.maxWidth * 0.05,
-                            ),
-                            SizedBox(
-                              width: constraints.maxWidth * 0.5,
-                              child: Text(
-                                state.cast[index].name,
-                                overflow: TextOverflow.ellipsis,
-                                style: AppStyles.cardButton.copyWith(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            )
-                          ]),
-                        ),
+                      return CastListviewItem(
+                        cast: state.cast[index],
                       );
                     },
-                  )
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isExpanded = !isExpanded;
+                      });
+                    },
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            isExpanded ? 'Show Less' : 'Show More',
+                            style: AppStyles.heading_3
+                                .copyWith(color: kSeconderyColor),
+                          ),
+                        ),
+                        Icon(
+                          isExpanded
+                              ? Icons.keyboard_arrow_up
+                              : Icons.keyboard_arrow_down,
+                          color: kSeconderyColor,
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             );
@@ -92,6 +96,47 @@ class MovieCast extends StatelessWidget {
             );
         }
       },
+    );
+  }
+}
+
+class CastListviewItem extends StatelessWidget {
+  const CastListviewItem({
+    super.key,
+    required this.cast,
+  });
+  final Cast cast;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => Container(
+        padding: EdgeInsets.all(constraints.maxWidth * 0.05),
+        decoration: BoxDecoration(
+          color: kWireframe_4,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Row(children: [
+          CircleAvatar(
+            radius: constraints.maxWidth * 0.15,
+            backgroundImage: CachedNetworkImageProvider(
+              ApiConstant.imageUrl(path: cast.profilePath),
+            ),
+          ),
+          SizedBox(
+            width: constraints.maxWidth * 0.05,
+          ),
+          SizedBox(
+            width: constraints.maxWidth * 0.5,
+            child: Text(
+              cast.name,
+              overflow: TextOverflow.ellipsis,
+              style: AppStyles.cardButton.copyWith(
+                color: Colors.white,
+              ),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
